9b053b2131e4b51ade4d4f3da0e0db5a93a3cd91
hardikshah197 <hardikshah.hs2015@gmail.com>
2021-06-01 23:39:47 +0530

    base url added

--- config/config.js
+++ config/config.js
@@ -3,19 +3,19 @@ module.exports = {
     development: {
         logDirectory: process.env.LOG_DIRECTORY || "./var/log/app",
         port:process.env.PORT || 4000,
-        base_url: process.env.BASE_URL || 'localhost:4000',
+        base_url: process.env.BASE_URL || 'http://localhost:4000',
         mongoURI: process.env.URI || 'http://localhost:27017',
     },
     test: {
         logDirectory: process.env.LOG_DIRECTORY || "./var/log/app",
         port:process.env.PORT || 4000,
-        base_url: process.env.BASE_URL || 'localhost:4000',
+        base_url: process.env.BASE_URL || 'http://localhost:4000',
         mongoURI: process.env.URI|| 'http://localhost:27017',
     },
     production : {
         logDirectory: process.env.LOG_DIRECTORY || "./var/log/app",
         port:process.env.PORT || 4000,
-        base_url: process.env.BASE_URL || 'localhost:4000',
+        base_url: process.env.BASE_URL || 'http://localhost:4000',
         mongoURI: process.env.URI|| 'http://localhost:27017',
     }
 }
